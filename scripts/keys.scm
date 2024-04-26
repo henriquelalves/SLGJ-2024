526f281cc2d1da11d0104de510c5bc301f605ddd
Henrique Alves <henrique.alves@itsjungle.xyz>
2024-04-26 14:53:39 +0300

    update

---
+++ scripts/keys.scm
@@ -0,0 +1,3 @@
+(define KEY_RIGHT 262)
+(define KEY_LEFT 263)
+(define KEY_DOWN 264)
--- scripts/main.scm
+++ scripts/main.scm
@@ -1,14 +1,16 @@
+(load "./scripts/keys.scm")
+
 (define a 0)
 (define b (rl-load-texture))
 
 (define (update)
-  (cond ((rl-is-key-down 262) (set! b (rl-load-texture))) ;; right
-	((rl-is-key-down 263) (set! a (+ a 1))) ;; left
-	((rl-is-key-down 264) (gc)) ;; down
+  (cond ((rl-is-key-down KEY_RIGHT) (set! b (rl-load-texture))) ;; right
+	((rl-is-key-down KEY_LEFT) (set! a (+ a 1))) ;; left
+	((rl-is-key-down KEY_DOWN) (gc)) ;; down
 	)
   )
 
 (define (draw)
   (rl-draw-text (number->string a))
-;    (rl-draw-texture b)
+  (rl-draw-texture b)
   )
